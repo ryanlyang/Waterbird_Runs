fe06a73ca389a8c5697244993ff9cb12475b6501
ryanlyang <ryan_l_yang@brown.edu>
2026-02-24 14:47:20 -0500

    oh yeah

--- GALS/RedMeat_Runs/run_guided_redmeat_gals_rn50att_sweep_optimnum.sh
+++ GALS/RedMeat_Runs/run_guided_redmeat_gals_rn50att_sweep_optimnum.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=reu-aisocial
 #SBATCH --partition=tier3
 #SBATCH --gres=gpu:a100:1
-#SBATCH --time=5-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
