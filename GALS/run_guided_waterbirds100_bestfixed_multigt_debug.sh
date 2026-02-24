d2c37f6292776f6019ea7bd5335a6765b7258b15
ryanlyang <ryan_l_yang@brown.edu>
2026-02-23 19:42:46 -0500

    yeah

--- GALS/run_guided_waterbirds100_bestfixed_multigt_debug.sh
+++ GALS/run_guided_waterbirds100_bestfixed_multigt_debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH --account=reu-aisocial
-#SBATCH --partition=debug
+#SBATCH --partition=tier3
 #SBATCH --gres=gpu:a100:1
-#SBATCH --time=8-00:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
