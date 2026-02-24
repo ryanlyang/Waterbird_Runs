ce2d9001e39af6e12d386b03d800e5ef4bbc3c0c
ryanlyang <ryan_l_yang@brown.edu>
2026-02-23 23:20:06 -0500

    good

--- GALS/run_afr_waterbirds100_repro_debug.sh
+++ GALS/run_afr_waterbirds100_repro_debug.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=reu-aisocial
 #SBATCH --partition=debug
 #SBATCH --gres=gpu:a100:1
-#SBATCH --time=8-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
