9b18a568a4b6b596237aea90f1c1a2237ff36772
ryanlyang <ryan_l_yang@brown.edu>
2026-03-02 14:53:59 -0500

    buh

--- GALS/run_clip_lr_wb95_wb100_decoy_sweep_optuna50.sh
+++ GALS/run_clip_lr_wb95_wb100_decoy_sweep_optuna50.sh
@@ -9,10 +9,10 @@
 #SBATCH --account=reu-aisocial
 #SBATCH --partition=tier3
 #SBATCH --gres=gpu:a100:1
-#SBATCH --time=5-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
 #SBATCH --output=/home/ryreu/guided_cnn/logsWaterbird/clip_lr_all3_optuna50_%j.out
 #SBATCH --error=/home/ryreu/guided_cnn/logsWaterbird/clip_lr_all3_optuna50_%j.err
 #SBATCH --signal=TERM@120
